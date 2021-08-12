df8c6bd38d3dbeb72f3c17ca6efb04c8c6590072
Gunnar Ulatowski <gunnarulatowski@outlook.com>
2021-08-11 21:36:40 -0400

    styling and added cards to house my projects

--- gnureactportfolio/src/components/Portfolio.js
+++ gnureactportfolio/src/components/Portfolio.js
@@ -3,14 +3,60 @@ import React from 'react'
 function portfolio() {
     return (
         <div>
-         <div className="card" style="width: 18rem;">
-  <img className="card-img-top" src="" alt="Card image cap"></img>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+            <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
+        <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
+        <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
+        <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
+        <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
+        <div className="card">
+         <img src="./movieshelf.png" alt="Movie Shelf Logo" style={{height: "110px",
+        width: "270px",
+        paddingLeft: "50px",
+        paddingTop: "50px",
+        }}></img>
+        <p style={{paddingLeft: "60px", paddingTop: "30px"}}>Link To Deployed App:</p>
+        <p style={{paddingLeft: "60px",}}>Link To Repository:</p>
+        </div>
         </div>
     )
 }
